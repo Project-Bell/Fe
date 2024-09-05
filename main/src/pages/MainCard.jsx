6e3ed9ae4444cf8e25689a1f43211c5f099b20c4
Forhye <onlyh4425@gmail.com>
2024-09-05 22:39:28 +0900

    ✅ add : 메인 카드 영역

---
+++ main/src/pages/MainCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+import "./MainCard.css";
+
+export default function MainCard() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}

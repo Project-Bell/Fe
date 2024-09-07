78e7bf930bbbcfe98547a155819f21e782c6b6b3
Forhye <onlyh4425@gmail.com>
2024-09-07 21:41:36 +0900

    feat : createCard 컴포넌트 연결

--- main/src/pages/MainCard.jsx
+++ main/src/pages/MainCard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import "./MainCard.css";
+import CreateCard from "../components/CreateCard";
 
 export default function MainCard() {
     return (
-        <>
+        <main className="main_card_container">
+            <h1 className="sr-only">메인 카드 영역</h1>
             <Header />
-        </>
+            <CreateCard />
+        </main>
     );
 }

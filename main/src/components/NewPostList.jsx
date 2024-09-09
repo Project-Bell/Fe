080be71af22d3992d05242f6ecc1d2b7781903de
Forhye <onlyh4425@gmail.com>
2024-09-09 21:25:19 +0900

    add : 최신글(new post) 리스트 컴포넌트 생성 및 하위 컴포넌트 연결

---
+++ main/src/components/NewPostList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NewPostList.css";
+import NewPost_txtType from "./NewPost_txtType";
+import NewPost_imgType from "./NewPost_imgType";
+
+export default function NewPostList() {
+    return (
+        <section className="new_post">
+            <h3>최신글</h3>
+            <NewPost_txtType />
+            <NewPost_imgType />
+        </section>
+    );
+}

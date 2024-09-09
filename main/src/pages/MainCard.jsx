e0cd889978fc5004903af15585340177ce3e8a1d
Forhye <onlyh4425@gmail.com>
2024-09-09 21:26:47 +0900

    feat : 핫이슈벨, 최신글 컴포넌트 연결

--- main/src/pages/MainCard.jsx
+++ main/src/pages/MainCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header";
 import "./MainCard.css";
 import CreateCard from "../components/CreateCard";
+import HotIssueList from "../components/HotIssueList";
+import NewPostList from "../components/NewPostList";
 
 export default function MainCard() {
     return (
@@ -9,6 +11,8 @@ export default function MainCard() {
             <h1 className="sr-only">메인 카드 영역</h1>
             <Header />
             <CreateCard />
+            <HotIssueList />
+            <NewPostList />
         </main>
     );
 }

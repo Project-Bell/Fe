074cf03f7b848ad34019530d12ebcee09482b4e7
Forhye <onlyh4425@gmail.com>
2024-09-14 19:49:28 +0900

    add : router 세팅 및 사이드 메뉴 컴포넌트 연결

--- main/src/App.js
+++ main/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainCard from "./pages/MainCard";
+import SideMenu from "./pages/SideMenu";
 
 function App() {
     return (
         <>
             <MainCard />
+            <SideMenu />
         </>
     );
 }

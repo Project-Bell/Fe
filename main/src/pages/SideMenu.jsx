428d4a7d1bb096a8c250aed3f6a21188fa1c9b49
Forhye <onlyh4425@gmail.com>
2024-09-14 19:51:56 +0900

    add : 사이드메뉴 컴포넌트 생성

---
+++ main/src/pages/SideMenu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UserHeader from "../components/UserHeader";
+
+export default function SideMenu() {
+    return (
+        <>
+            <UserHeader />
+        </>
+    );
+}

205034d9c5a860a8174fcf137499a3457682e299
Forhye <onlyh4425@gmail.com>
2024-09-14 19:51:28 +0900

    feat : html element 수정 main -> section

--- main/src/pages/MainCard.jsx
+++ main/src/pages/MainCard.jsx
@@ -7,12 +7,13 @@ import NewPostList from "../components/NewPostList";
 
 export default function MainCard() {
     return (
-        <main className="main_card_container">
+        <section className="main_card_container">
             <h1 className="sr-only">메인 카드 영역</h1>
+
             <Header />
             <CreateCard />
             <HotIssueList />
             <NewPostList />
-        </main>
+        </section>
     );
 }

bf4bcb35566dd4eae51eb70abfd176eb2ce12ff6
Forhye <onlyh4425@gmail.com>
2024-09-20 17:24:25 +0900

    fix : 경고창내용 수렴 수정 + 스타일 컴포넌트를 함수와 분리 및 네이밍 수정

--- main/src/components/components_btn/LiveBellTop2.jsx
+++ main/src/components/components_btn/LiveBellTop2.jsx
@@ -1,33 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function LiveBellTop2({ topTopic }) {
-    const LiveBellTop2 = styled.button`
-        color: #fff;
-        font-size: 28px;
-        font-weight: 700;
-        text-align: center;
+const LiveBell2 = styled.button`
+    color: #fff;
+    font-size: 28px;
+    font-weight: 700;
+    text-align: center;
 
-        width: 272px;
-        height: 135px;
-        border: none;
+    width: 272px;
+    height: 135px;
+    border: none;
+    border-radius: 10px;
+    background-image: url("https://picsum.photos/seed/picsum/536/354");
+    background-size: cover;
+
+    position: relative;
+    isolation: isolate;
+
+    &::after {
+        content: "";
+        position: absolute;
+        background: black;
+        z-index: -1;
+        inset: 0;
+        opacity: 0.8;
         border-radius: 10px;
-        background-image: url("https://picsum.photos/seed/picsum/536/354");
-        background-size: cover;
-
-        position: relative;
-        isolation: isolate;
-
-        &::after {
-            content: "";
-            position: absolute;
-            background: black;
-            z-index: -1;
-            inset: 0;
-            opacity: 0.8;
-            border-radius: 10px;
-        }
-    `;
-
-    return <LiveBellTop2 type="button">{topTopic}</LiveBellTop2>;
+    }
+`;
+export default function LiveBellTop2({ topTopic }) {
+    return <LiveBell2 type="button">{topTopic}</LiveBell2>;
 }
--- main/src/components/components_btn/LiveBellTop8.jsx
+++ main/src/components/components_btn/LiveBellTop8.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function LiveBellTop8({ topTopic }) {
-    const LiveBellTop8 = styled.button`
-        color: #fff;
-        font-size: 26px;
-        text-align: center;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
+const LiveBell8 = styled.button`
+    color: #fff;
+    font-size: 26px;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 
-        width: 135px;
-        height: 135px;
-        border: none;
+    width: 135px;
+    height: 135px;
+    border: none;
+    border-radius: 10px;
+    background-image: url("https://picsum.photos/seed/picsum/536/354");
+    background-size: cover;
+
+    position: relative;
+    isolation: isolate;
+
+    &::after {
+        content: "";
+        position: absolute;
+        background: black;
+        z-index: -1;
+        inset: 0;
+        opacity: 0.8;
         border-radius: 10px;
-        background-image: url("https://picsum.photos/seed/picsum/536/354");
-        background-size: cover;
-
-        position: relative;
-        isolation: isolate;
-
-        &::after {
-            content: "";
-            position: absolute;
-            background: black;
-            z-index: -1;
-            inset: 0;
-            opacity: 0.8;
-            border-radius: 10px;
-        }
-    `;
-
-    return <LiveBellTop8 type="button">{topTopic}</LiveBellTop8>;
+    }
+`;
+export default function LiveBellTop8({ topTopic }) {
+    return <LiveBell8 type="button">{topTopic}</LiveBell8>;
 }

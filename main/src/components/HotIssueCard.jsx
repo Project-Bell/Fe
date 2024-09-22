3f711661e2802f99e0b45f76ef3876e072957cdf
Forhye <onlyh4425@gmail.com>
2024-09-22 20:06:18 +0900

    feat : 카드 컨텐츠 내용을 데이터화 하여 전달

--- main/src/components/HotIssueCard.jsx
+++ main/src/components/HotIssueCard.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 import "./HotIssueCard.css";
 
-export default function HotIssueCard({ titleData }) {
+export default function HotIssueCard({ titleData, contentData }) {
     return (
         <div className="hotIssue_card">
             <h4>{titleData}</h4>
-            <p>
-                계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무
-                걱정도 없이 가을 속의 별들을 다 헬 듯합니다. 가슴 속에 하나 둘
-                새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,
-                내일 밤이 남은 까닭이요, 아직 나의 청춘이 다하지 않은
-                까닭입니다. 별 하나에 추억과 별 하나에 사랑과 별 하나에 쓸쓸함과
-                별 하나에 동경과 별 하나에 시와 별 하나에 어머니, 어머니,
-                어머님, 나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다. 소학교
-                때 책상을 같이 했던 아이들의 이름과 패, 경, 옥 이런 이국소녀 패,
-                경, 옥 이런 이국소녀
-            </p>
+            <p>{contentData}</p>
         </div>
     );
 }
--- main/src/components/HotIssueList.jsx
+++ main/src/components/HotIssueList.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import HotIssueCard from "./HotIssueCard";
 import "./HotIssueList.css";
 
+const mokdata =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum blanditiis beatae aut id distinctio dolore, architecto commodi quod non corporis alias ullam hic molestiae impedit dicta fuga. Adipisci, consequatur molestiae.";
+
 export default function HotIssueList() {
     return (
         <section className="hot_issue">
             <h3> Hot Issue Bell</h3>
             <div className="card_list">
-                <HotIssueCard titleData={"핫이슈 1"} />
-                <HotIssueCard titleData={"핫이슈 2"} />
-                <HotIssueCard titleData={"핫이슈 3"} />
-                <HotIssueCard titleData={"핫이슈 4"} />
+                <HotIssueCard titleData={"핫이슈 1"} contentData={mokdata} />
+                <HotIssueCard titleData={"핫이슈 2"} contentData={mokdata} />
+                <HotIssueCard titleData={"핫이슈 3"} contentData={mokdata} />
+                <HotIssueCard titleData={"핫이슈 4"} contentData={mokdata} />
             </div>
         </section>
     );

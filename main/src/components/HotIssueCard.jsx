017e37d9d825b80a9c7e0668960d8c940efda0cd
Forhye <onlyh4425@gmail.com>
2024-09-25 21:44:07 +0900

    style : 갤러리 아이콘 추가

--- main/src/components/HotIssueCard.jsx
+++ main/src/components/HotIssueCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./HotIssueCard.css";
+import { FaRegImage } from "react-icons/fa";
 
 export default function HotIssueCard({ titleData, contentData }) {
     return (
         <div className="hotIssue_card">
             <h4>{titleData}</h4>
+            <span className="hotIssue_gallery">
+                <FaRegImage size="10px" color="#777" />
+            </span>
             <p>{contentData}</p>
         </div>
     );

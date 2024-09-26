1c436302bd3b49128a9266df27d6842785267c13
Forhye <onlyh4425@gmail.com>
2024-09-26 21:50:28 +0900

    style : 텍스트 정렬

--- main/src/components/components_btn/WhiteBtn.jsx
+++ main/src/components/components_btn/WhiteBtn.jsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 
 const WhiteBtnStyled = styled.button`
     font-size: 1.125rem;
-    text-align: center;
+    text-align: left;
 
     width: 270px;
     height: 50px;
     border: 1px solid #eee;
     border-radius: 10px;
+    padding-left: 50px;
 `;
 
 export default function WhiteBtn({ pageName, pageLink }) {
